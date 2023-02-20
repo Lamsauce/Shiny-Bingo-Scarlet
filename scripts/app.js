53f628d287b7c2821a0391d5f8941a8abb9d4572
Lily Lam <lilylam@fb.com>
2023-02-19 21:17:23 -0800

    added random tile generator

--- scripts/app.js
+++ scripts/app.js
@@ -7,4 +7,16 @@ const found = function found() {
     });
 }
 
+const tiles = ["B1", "B2", "B3", "B4", "B5", "I1", "I2", "I3", "I4", "I5", "N1", "N2", "N3", "N4", "N5", "G1", "G2", "G3", "G4", "G5", "O1", "O2", "O3", "O4", "O5"];
+function random_tile(){
+    $(document).ready(function() {
+        $('#generate').click(function(event){
+            let int = Math.floor(Math.random() * 24);
+            console.log(tiles[int]);
+            $('#generated-tile').text(tiles[int]);
+        }); 
+    });
+}
+
 found();
+random_tile();
