c262c68917b4bdbec22338caf90e57337dba17fc
Lily Lam <lilylam@fb.com>
2023-02-19 21:40:02 -0800

    feature that removes generated tiles from array

--- scripts/app.js
+++ scripts/app.js
@@ -7,13 +7,16 @@ const found = function found() {
     });
 }
 
-const tiles = ["B1", "B2", "B3", "B4", "B5", "I1", "I2", "I3", "I4", "I5", "N1", "N2", "N3", "N4", "N5", "G1", "G2", "G3", "G4", "G5", "O1", "O2", "O3", "O4", "O5"];
+let tiles = ["B1", "B2", "B3", "B4", "B5", "I1", "I2", "I3", "I4", "I5", "N1", "N2", "N3", "N4", "N5", "G1", "G2", "G3", "G4", "G5", "O1", "O2", "O3", "O4", "O5"];
+
 function random_tile(){
     $(document).ready(function() {
         $('#generate').click(function(event){
-            let int = Math.floor(Math.random() * 24);
-            console.log(tiles[int]);
+            let int = Math.floor(Math.random() * tiles.length);
             $('#generated-tile').text(tiles[int]);
+            tiles.splice(int,1);
+            console.log(tiles.length);
+            console.log(tiles);
         }); 
     });
 }
